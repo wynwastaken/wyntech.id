c108170d72873849d1741a0e8908c01f05888352
wynwastaken <delwyn.fayad@gmail.com>
2025-09-08 21:24:17 +0700

    faq

---
+++ test/test3/test3.js
@@ -0,0 +1,16 @@
+
+
+const isi_faqs = document.querySelectorAll('.isi-faq');
+const arrow_faqs = document.querySelectorAll('.open-faq');
+
+arrow_faqs.forEach(function(arrow,index){
+    arrow.addEventListener('click',function(){
+    if(arrow_faqs[index].classList.contains('rotate')){
+        arrow_faqs[index].classList.remove('rotate');
+        isi_faqs[index].classList.remove('resize');
+    }else{
+        arrow_faqs[index].classList.add('rotate');
+        isi_faqs[index].classList.add('resize');
+    }
+});
+})
