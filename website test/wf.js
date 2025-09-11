5bcef83ebe51b09297f15a8631b0437c24b17f99
wynwastaken <delwyn.fayad@gmail.com>
2025-09-11 08:09:35 +0700

    show-password

--- website test/wf.js
+++ website test/wf.js
@@ -5,11 +5,14 @@ const head = document.querySelector('.head');
 const click_audio = document.getElementById('click-sound-effect');
 const buttons = document.querySelectorAll('.button');
 const sign_in_page =document.querySelector('.sign-in-page');
+const sign_in_form = document.querySelector('.sign-in-form');
 const warning_username = document.getElementById('required-usernamel');
 const warning_password = document.getElementById('length-passwordl');
 const input_username = document.getElementById('username');
 const input_password = document.getElementById('password');
-
+const  show = document.querySelectorAll('.show-icon');
+const create_link = document.getElementById('createl');
+const sign_up_form = document.querySelector('.sign-up-form');
 
 hamburger.addEventListener('click',function(){
     if(menu_bar.classList.contains('show')){
@@ -57,3 +60,23 @@ input_password.addEventListener('input',function(){
         input_password.classList.remove('warning');
     }
     });
+
+
+show.forEach(function(el){
+    el.addEventListener('click',function(){
+    if(input_password.type === "password"){
+        input_password.type = "text";
+        show.src = "unshow.png";
+    }else{
+        input_password.type = "password";
+        show.src = "show.png";
+    }
+    });
+})
+
+
+create_link.addEventListener('click',function(){
+    sign_in_form.classList.add('hilang');
+    sign_up_form.classList.add('muncul');
+});
+
