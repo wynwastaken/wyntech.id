c07da4b4590008463910e2c45e3fea7c425ac679
wynwastaken <delwyn.fayad@gmail.com>
2025-09-21 13:45:48 +0700

    update

--- website test/produk/wf.js
+++ website test/produk/wf.js
@@ -1,8 +1,6 @@
-const hamburger = document.querySelector('.hamburger');
-const menu_bar = document.querySelector('.menu-bar');
-const menu_head = document.querySelector('.head-menu');
+
 const sign_ins = document.querySelectorAll('.sign-in');
-const head = document.querySelector('.head');
+
 const click_audio = document.getElementById('click-sound-effect');
 const buttons = document.querySelectorAll('.button');
 const sign_in_page =document.querySelector('.sign-in-page');
@@ -20,14 +18,11 @@ const warning_email2= document.getElementById('required-emaill2');
 const  show = document.querySelectorAll('.show-icon');
 const create_link = document.getElementById('createl');
 const sign_up_form = document.querySelector('.sign-up-form');
-const return_arrow1 = document.querySelector('.back1');
-const menu_produk = document.querySelector('.halaman');
 const already_have = document.querySelector('.cl2');
 
-const scroll_up = document.querySelector('.scroll-up');
-const container_scroll_up = document.querySelector('.container-scroll-up');
 
-let isScrollListenerActive = true;
+
+
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 input_email.addEventListener('input', function () {
   if (!emailRegex.test(input_email.value.trim())) {
@@ -50,38 +45,12 @@ input_email2.addEventListener('input', function () {
   }
 });
 
-hamburger.addEventListener('click',function(){
-    if(menu_head.classList.contains('show')){
-        menu_head.classList.remove('show');
-    }else{
-        menu_head.classList.add('show');
-    }
-    
-});
 
 
 
 
-window.addEventListener('scroll',function (){
-    if(!isScrollListenerActive) return;
-    
-    const PositionWindow = window.scrollY;
-    const PositionScrollShow = menu_produk.getBoundingClientRect().bottom;
-    if(PositionWindow >= PositionScrollShow){
-        scroll_up.classList.remove('hilang-smooth');
-    }else{
-        scroll_up.classList.add('hilang-smooth');
-    }
-});
 
-scroll_up.addEventListener('click',function(){
-    if(!scroll_up.classList.contains('hilang-smooth')){
-        window.scrollTo({
-            top:0,
-            behavior:'smooth'
-        })
-    }
-});
+
 
 sign_ins.forEach(function(el){
     el.addEventListener('click',function(){
@@ -149,11 +118,11 @@ show.forEach(function(el){
     if(input_password.type === "password"){
         input_password.type = "text";
         input_password2.type = "text";
-        el.src = "../unshow.png";
+        el.src = "unshow.png";
     }else{
         input_password.type = "password";
         input_password2.type = "password";
-        el.src = "../show.png";
+        el.src = "show.png";
     }
     })
 });
@@ -170,16 +139,7 @@ already_have.addEventListener('click',function(){
 });
 
 
-return_arrow1.addEventListener('click',function(){
-    container_scroll_up.classList.remove('hilang-smooth');
-    isScrollListenerActive = true;
-    sign_in_page.classList.remove('muncul');
-    document.body.style.overflowY = 'auto'; 
-    setTimeout(function() {
-        menu_produk.classList.remove('hilang');
-        menu_produk.classList.remove('hilang-smooth');
-    }, 50);
-});
+
 
 
 
