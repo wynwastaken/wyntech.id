0f23258f681d1098bd7a6b8f77d76adea3b7e914
wynwastaken <delwyn.fayad@gmail.com>
2025-10-06 18:29:15 +0700

    save

--- website test/sign-in/index.php
+++ website test/sign-in/index.php
@@ -45,7 +45,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>  
+    <title>wfdigistore</title>  
     <link rel="stylesheet" href="sign-in.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
