cb446c7e86281cee0f71b72aed971d7bd00dadec
wynwastaken <delwyn.fayad@gmail.com>
2025-10-06 07:53:11 +0700

    save

--- website test/produk/index.php
+++ website test/produk/index.php
@@ -191,9 +191,9 @@
 
                     <?php foreach($results as $each): ?>
                         
-                        <div class="gambar_produk">
-                            <img src="<?php echo $each['path_produk']; ?>" alt="<?php echo $each['nama_produk']; ?>" class="img-produk">
-                        </div>
+                        
+                        <img src="<?php echo $each['path_produk']; ?>" alt="<?php echo $each['nama_produk']; ?>" class="kotak_produk">
+                        
                                 
                         
                     <?php endforeach; ?>
